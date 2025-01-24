8eb69b8cd681547e366abe1754ca169051678db6
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2025-01-23 20:43:24 -0800

    Refactor toFullPath function for improved error handling and path conversion
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- helpers_windows.go
+++ helpers_windows.go
@@ -10,21 +10,24 @@ import (
 )
 
 func toFullPath(s string) (string, error) {
-	p := syscall.StringToUTF16(s)
-	b := p
-	n, err := syscall.GetLongPathName(&p[0], &b[0], uint32(len(b)))
+	p, err := syscall.UTF16FromString(s)
 	if err != nil {
-		return s, err
-	}
-	if n > uint32(len(b)) {
-		b = make([]uint16, n)
-		n, err = syscall.GetLongPathName(&p[0], &b[0], uint32(len(b)))
+		b := p
+		n, err := syscall.GetLongPathName(&p[0], &b[0], uint32(len(b)))
 		if err != nil {
 			return s, err
 		}
+		if n > uint32(len(b)) {
+			b = make([]uint16, n)
+			n, err = syscall.GetLongPathName(&p[0], &b[0], uint32(len(b)))
+			if err != nil {
+				return s, err
+			}
+		}
+		b = b[:n]
+		return syscall.UTF16ToString(b), nil
 	}
-	b = b[:n]
-	return syscall.UTF16ToString(b), nil
+	return s, nil
 }
 
 func evalSymlinks(path string) (string, error) {
